bf453e59abb5217ac4b5b93b7ad9cad27d00df14
PlanetEfficacy <jspevack@gmail.com>
2016-10-19 09:36:49 -0600

    get working query

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,5 @@
 class SearchController < ApplicationController
   def index
-    # https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest?location=80203&api_key=I6JZ4q6YZSgnPLrcuOM3smCSaG80m2YFHkmyo2uq&format=JSON
+    # https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=LPG,ELEC&zip=80203&limit=10&radius=6.0&limit=10&api_key=I6JZ4q6YZSgnPLrcuOM3smCSaG80m2YFHkmyo2uq&format=JSON
   end
 end
